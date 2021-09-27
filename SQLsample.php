9c9ac903873a7400ad401ee4910ad2a648f4d2b2
rsx0806 <42129362+rsx0806@users.noreply.github.com>
2021-09-27 11:45:13 +0800

    Create SQLsample.php

---
+++ SQLsample.php
@@ -0,0 +1,42 @@
+    function getAll($params)
+    {
+        $output = [];
+        $status = "!=";
+        if(isset($params['city']) && $params['city'] != ""){
+            $status = "=";
+        }
+        $subdiv = "17";
+        if(isset($params['podr']) && $params['podr'] != ""){
+            $status = "=";
+            switch ($params['podr']) {
+                case 'grt':
+                    $subdiv = '26';
+                    break;
+                case 'mk':
+                    $subdiv = '24';
+                    break;
+                default:
+                    $subdiv = '17';
+                    break;
+            }
+        }
+        $sql = "select 
+        -db_name-.-param-,-db_name-.-param-,-db_name-.-param-, -db_name-.-param-, -db_name-.-param-
+        from -db_name-
+        left join -db_name- on -db_name-.-param- = -db_name-.-param-
+        left join -db_name- on -db_name-.-param- = -db_name-.-param-
+        left join -db_name- on -db_name-.-param- = -db_name-.-param-
+        left join -db_name- on -db_name-.-param- = -db_name-.-param-
+        where -db_name-.id in ( 
+        select id from -db_name-
+             left join -db_name- on -db_name-.-param- = -db_name-.id and -db_name-.-param- = 31 where
+             -param- = false and -db_name-.-param- = true
+        )
+        and -db_name-.-param- ".$status.$subdiv." group by -db_name-.-param- order by -db_name-.-param-;";
+        $recordset = $this->con->query($sql);
+        while ($record = $recordset->fetch())
+        {
+            $output[] = $record;
+        }
+        return $output;
+    }
